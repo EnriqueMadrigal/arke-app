33940d01ed1ad6eb56b8204eace7b12581823d26
Enrique Madrigal <emadriga@gmail.com>
2018-09-14 13:45:40 -0500

    Nuevos

--- app/src/main/java/com/datalabor/soporte/arke/activity/login.java
+++ app/src/main/java/com/datalabor/soporte/arke/activity/login.java
@@ -1,13 +1,14 @@
-package com.datalabor.soporte.arke;
+package com.datalabor.soporte.arke.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.datalabor.soporte.arke.R;
 
-public class MainActivity extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
